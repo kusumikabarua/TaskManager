284ce62ed0c5815220bfa3288d275c4e520fa5b3
KB <kb@prashants-MacBook-Pro-2.local>
2024-03-12 18:40:47 +0530

    3nd Session Commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const cors =require("cors");
+app.use(cors());
 app.use(express.json());
+
 app.use("/api/tasks",taskRoutes);
 app.use("/api/auth",authRoutes);
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
