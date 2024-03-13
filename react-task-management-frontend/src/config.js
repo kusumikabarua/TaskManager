646fffa1f93b89f652e895e756a630fdf03b9c87
KB <kb@prashants-MacBook-Pro-2.local>
2024-03-13 18:00:58 +0530

    Backend URL added in config.js

--- react-task-management-frontend/src/config.js
+++ react-task-management-frontend/src/config.js
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "http://localhost:4000/api";
-export const API_BASE_URL = "https://taskmanager-9vhd.onrender.com"
+export const API_BASE_URL = "https://taskmanager-9vhd.onrender.com/api"
